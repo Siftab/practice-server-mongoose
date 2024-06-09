40f7f3a1be39b2f53e8d9d99011a9be62a7ede5a
Siftab <siiffuuu@gmail.com>
2024-06-10 00:41:06 +0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express, { Application, Request, Response } from "express"
 export const app: Application = express()
+import cors from "cors"
 const port = 3000
+
+//parsen
+app.use(express.json())
+app.use(express.text())
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
+})
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.send("i am hitted")
 })
 
